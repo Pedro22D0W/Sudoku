da85974838b5a0b356ae77bcacf7574a4b924fd5
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2022-11-29 22:40:50 -0300

    entrada

--- suduko2.asm
+++ suduko2.asm
@@ -7,59 +7,84 @@ PL  MACRO
 
     
     ENDM
+.stack 100h
 .DATA 
 
-matr1 DB 35h,33h,?
-      db 36h,35h,?
-      db ?,39h,38h
 
-matr2 db ?,37h,?
-      db 31h,39h,35h
-      db ?,?,?
+msg1 db "#|012345678$"
+msg2 db "------------$"
+msg3 db "|$"
+matriz db 35h,33h,0,0,37h,0,0,0,0
+       db 36h,?,?,31h,39h,35h,?,?,?
+       db ?,39h,38h,?,?,?,?,36h,?
+       db 38h,?,?,?,36h,?,?,?,33h
+       db 34h,?,?,38h,?,33h,?,?,31h
+       db 37h,?,?,?,32h,?,?,?,36h
+       db ?,36h,?,?,?,?,32h,38h,?
+       db ?,?,?,34h,31h,39h,?,?,35h
+       db ?,?,?,?,38h,?,?,37h,39h
 
-matr3 db ?,?,?
-      db ?,?,?
-      db ?,36h,?
-
-matr4 db 38h,?,?
-      db 34h,?,?
-      db 37h,?,?
-
-matr5 db ?,36h,?
-      db 38h,?,33h
-      db ?,32h,?
-
-matr6 db ?,?,33h
-      db ?,?,31h
-      db ?,?,36h
-
-matr7 db ?,36h,?
-      db ?,?,?
-      db ?,?,?
-         
-matr8 db ?,?,?
-      db 34h,31h,39h
-      db ?,38h,?
-
-matr9 db 32h,38h,?
-      db ?,?,35h
-      db ?,37h,39h
-
-.CODE
+.CODE 
 
 
 main PROC
     mov ax,@data
     mov ds,ax
     mov es,ax
+    lea SI,matriz
+    volta2:
+    xor si,si
+    xor bx,bx
+    mov cl,73
     
-    mov bx,1
-    mov si,3
+    mov ah,09 
+    lea dx,msg1
+    int 21h
+    pl
+     mov ah,09 
+    lea dx,msg2
+    int 21h
+    pl
+    mov ch,30h
+    mov ah,02
+    mov dl,ch
+    int 21h
+    mov ah,09 
+    lea dx,msg3
+    int 21h
+    volta:
+    
+    mov dl,matriz[si]
+    mov ah,02
+     int 21h
+    inc si
+    inc bx
+    cmp cx,1
+    je fim
+    cmp bx,9
+    je pl1
+    loop volta
+    jmp fim
+    pl1:
+    PL
+    add ch,01h
+    xor bx,bx
+    cmp ch,39h
+    je fim
+    mov ah,02 
+    mov dl,ch
+    int 21h
+      mov ah,09 
+    lea dx,msg3
+    int 21h
+
+
+   
 
     
-    mov dl,matr1[bx][si]
-    mov ah,02
-    int 21h
+    jmp volta
+
+    
     
    
 
@@ -67,6 +92,39 @@ main PROC
 
 
 
+    fim:
+    pl
+    pl
+   
+      xor si,si
+      xor bx,bx
+
+      mov bl,9
+      mov ah,01
+      int 21h
+      sub al,30h
+      mul bl
+      mov bl,al
+
+      mov ah,01
+      int 21h
+      sub al,30h
+      xor si,si
+      xor ah,ah
+      mov si,ax
+
+      mov ah,01
+      int 21h
+
+    xor ah,ah
+    mov matriz[bx][si],al
+    pl
+    jmp volta2
+   
+
+   
+
+    
 
     mov ah,4ch
     int 21h
